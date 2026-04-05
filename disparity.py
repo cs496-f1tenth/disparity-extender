1a29842efa65b2893c35ca64b34628d560dad378
taco <brylejaronong@gmail.com>
2026-04-05 05:38:44 -0700

    multivehicle support

--- disparity.py
+++ disparity.py
@@ -9,12 +9,12 @@ from nav_msgs.msg import Odometry
 
 class DisparityExtender(Node):
 
-    CAR_WIDTH = 1.0
+    CAR_WIDTH = 0.27
     DIFFERENCE_THRESHOLD = 2.
     STRAIGHTS_SPEED = 1.0
     CORNERS_SPEED = 1.0
     DRAG_SPEED = 1.0
-    SAFETY_PERCENTAGE = 900.
+    SAFETY_PERCENTAGE = 3.00
 
     def __init__(self):
         super().__init__('disparity_extender_node')
@@ -29,16 +29,39 @@ class DisparityExtender(Node):
         self.speed = 1.0  # Initial speed
         self.radians_per_point = 0.0
 
-        lidarscan_topic = '/scan'
-        odom_topic = '/vesc/odom'
-        drive_topic = '/vesc/low_level/ackermann_cmd_mux/input/teleop'
+        # namespace
+        ns = self.get_namespace()
+        if ns == '/':
+            self.get_logger().warn(
+                "Node running in global namespace. "
+                "For multi-vehicle, run with __ns:=/vehicleX"
+            )
+
+        self.declare_parameter('odom_topic', 'odom')
+        self.declare_parameter('scan_topic', 'scan')
+        self.declare_parameter('drive_topic', 'drive')
+
+        odom_topic = self.get_parameter('odom_topic').value
+        scan_topic = self.get_parameter('scan_topic').value
+        drive_topic = self.get_parameter('drive_topic').value
 
         self.odom_sub = self.create_subscription(
             Odometry, odom_topic, self.odom_cb, 2)
         self.lidar_sub = self.create_subscription(
-            LaserScan, lidarscan_topic, self.process_lidar, 1)
+            LaserScan, scan_topic, self.process_lidar, 1)
         self.drive_pub = self.create_publisher(
             AckermannDriveStamped, drive_topic, 1)
+
+#        lidarscan_topic = '/scan'
+#        odom_topic = '/vesc/odom'
+#        drive_topic = '/vesc/low_level/ackermann_cmd_mux/input/teleop'
+#
+#        self.odom_sub = self.create_subscription(
+#            Odometry, odom_topic, self.odom_cb, 2)
+#        self.lidar_sub = self.create_subscription(
+#            LaserScan, lidarscan_topic, self.process_lidar, 1)
+#        self.drive_pub = self.create_publisher(
+#            AckermannDriveStamped, drive_topic, 1)
 
     def odom_cb(self, data):
         self.speed = data.twist.twist.linear.x
@@ -70,7 +93,7 @@ class DisparityExtender(Node):
         return ranges
 
     def extend_disparities(self, disparities, ranges, car_width, extra_pct):
-        width_to_cover = 0.155 * (1 + extra_pct / 100)
+        width_to_cover = car_width * extra_pct
         for index in disparities:
             first_idx = index - 1
             points = ranges[first_idx:first_idx + 2]
@@ -115,9 +138,9 @@ class DisparityExtender(Node):
         self.get_logger().info(f'x: {x}, speed: {speed}')
 
         #Makes the car backup and turn towards the goal point if there are no good paths.
-        if(x <= 2.0):
-            speed *= -1
-            steering_angle *= -1
+#        if(x <= 2.0):
+#            speed *= -1
+#            steering_angle *= -1
         
         drive_msg = AckermannDriveStamped()
         drive_msg.header.stamp = self.get_clock().now().to_msg()
